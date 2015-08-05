5c4e26b10baf70b442d6eb0e043c060930454064
patrickschu <patrickschu@users.noreply.github.com>
2015-08-05 14:23:13 -0500

    Update betterplotter.R

--- betterplotter.R
+++ betterplotter.R
@@ -2,7 +2,7 @@
 
 mentplotter=function(filename,decade) {
 ment=read.csv(file.choose(), header=T)
-i=c(8:length(colnames(ment)))
-setwd("~/Desktop")
-for (item in i) {print (colnames(ment)[item]); jpeg (paste(colnames(ment)[item],"_",decade, "s.jpg"));  formula=tapply(ment$menttotal, ment$pubdate, sum)/tapply(ment$wordcount, ment$pubdate, sum); plot(formula,xaxt="n", main=colnames(ment)[item], ylab="Mean Frequency per Word"); xnames=names(formula);axis(1,at=1:length(xnames), labels=xnames); dev.off()}
+i=c(10:length(colnames(ment)))
+setwd("~/Desktop/rplots")
+for (item in i) {print (colnames(ment)[item]); jpeg (paste(as.character(colnames(ment)[item]),"_",as.character(decade), "s.jpg"));  formula=tapply(ment[,item], ment$pubdate, sum)/tapply(ment$wordcount, ment$pubdate, sum); plot(formula*1000000,xaxt="n", main=colnames(ment)[item], ylab="Mean Frequency per Word"); xnames=names(formula);axis(1,at=1:length(xnames), labels=xnames); dev.off()}
 }
