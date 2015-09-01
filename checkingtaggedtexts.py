17a612c26130a8d23dcee21f35b7d15c42bbd31b
patrickschu <patrickschu@users.noreply.github.com>
2015-09-01 16:08:52 -0500

    Update checkingtaggedtexts.py

--- checkingtaggedtexts.py
+++ checkingtaggedtexts.py
@@ -1,8 +1,8 @@
 #this reads in the files and sees if tagging was successful
-import os; import codecs
+import os; import codecs; import shutil
 
 t=os.listdir("files_tobechecked")
-
+#read in and see which are failures
 failures=[]
 for item in t:
   f=codecs.open("files_tobechecked\\"+item, "r", "utf-8")
@@ -12,3 +12,7 @@ for item in t:
 	f.close()
 
 
+#move non-failures to happy place
+for item in t:
+	if item not in failures:
+		shutil.copy("files_tobechecked\\"+item, "files_good\\"+item)
