051f06c28885401689786b07704e124dc220c92f
patrickschu <patrickschu@users.noreply.github.com>
2016-06-20 13:17:14 -0500

    Add files via upload

--- types_0414.R
+++ types_0414.R
@@ -1,12 +1,11 @@
 # we plot types
 # how much fun is that?
 
-yeslist=read.csv("/Users/ps22344/Downloads/ota-master/yeslist_regex_610.txt", sep="\t", header=F)
+yeslist=read.csv("/Users/ps22344/Downloads/ota-master/yeslist_regex_620_16.txt", sep="\t", header=F)
 summary(yeslist)
 
 
-names(yeslist)=c('word','count', 'birth', 'suffix', 
-'something', "something_else", "different_thing", "weird_thing")
+names(yeslist)=c('word','birth')
 head(yeslist)
 
 summary(yeslist$birth)
@@ -20,6 +19,39 @@ t=xtabs(~fifties, data=yeslist)
 
 plot(t, xaxt="n", main="Nominalization per 50 years") 
 axis(side=1, at=c(1:9), labels=fifties)
+#per 25 years
+
+twens=seq(1400,1800, by= 25)
+length(twens)
+
+
+yeslist$twennies=findInterval (yeslist$birth, twens)
+
+t=data.frame(table(yeslist$twennies))
+names(t)=c("int", "freq")
+zeros=c(1,2,4,12,13,17)
+zero=0
+tt=data.frame("int"=zeros, "freq"=zero)
+
+final=rbind(t,tt)
+t$int=as.character(t$int)
+t
+summary(final)
+levels(as.factor(final$int))
+final=final[ order(as.numeric(final$int)), ]
+
+
+twens
+barplot(final$freq, ylab="Number of nouns created", main="Neologisms with -ment, 1400-1800", ylim=c(0,12), names.arg=c(twens)) 
+
+axis(side=1, at=c(1:17), labels=twens)
+
+print (length(twens))
+
+
+
+# barplot(t, type='h', lwd=15, lty=1,xaxt="n", ylab="Number of nouns created", main="Neologisms with -ment, 1400-1800", ylim=c(0,12)) 
+#axis(side=1, at=c(1:17), labels=twens)
 
 
 ##per 10 years
