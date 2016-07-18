f106b53b124c2f515b85593ae6a56e8ff0de6d1e
patrickschu <patrickschu@users.noreply.github.com>
2016-07-17 19:11:22 -0500

    Add files via upload

--- hapax_counter_0714.py
+++ hapax_counter_0714.py
@@ -20,6 +20,7 @@ now=time.time()
 
 
 yeslist=[]
+yesdict={}
 f=codecs.open("/Users/ps22344/Downloads/ota-master/paperstuff/alllist_0712_corrected_regex.txt_pandas_0to1700.txt", "r", "utf-8")
 for line in f:
 	yeslist.append(line.rstrip("\n").split("\t"))
@@ -28,7 +29,9 @@ f.close()
 
 #WATCH THIS SETTING
 yeslist_words=[i[1] for i in yeslist]
-print "yeslist", yeslist_words
+yesdict={i[1]:0 for i in yeslist}
+#print "yeslist", yeslist_words
+#print yesdict
 
 
 
@@ -61,11 +64,10 @@ def dictwriter(file_name, dictionary, sort_dict=True):
 	"""
 	print "Starting the dictionarywriter, sorting is", sort_dict
 	sorteddict=sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
-	print sorteddict
 	with codecs.open(file_name, "w", "utf-8") as outputi:
- 		outputi.write("\n".join([":".join([i[0],unicode(i[1])]) for i in sorteddict]))
-	with open(file_name+".json", "w") as jsonoutputi:
-		json.dump({k: v for k,v in dictionary.items()}, jsonoutputi,  ensure_ascii=False)
+		outputi.write("\n".join([":".join([i[0],unicode(i[1])]) for i in sorteddict]))
+	with codecs.open(file_name+".json", "w", "utf-8") as jsonoutputi:
+		json.dump(dictionary, jsonoutputi, ensure_ascii=False)
 	
 header="\n\n-------\n"
 	
@@ -82,34 +84,39 @@ def main(inputspread, start_time, end_time, interval):
 		print header, item, header
 		subsetspread= periodfinder(inputspread, item, interval) 
 		perioddicti= defaultdict(list)
+		perioddicti_ment=defaultdict()
 		#iterating over all the files contained in the extracted spreadsheet
-		for fileno in subsetspread['filenumber']:
+		for fileno in [f for f in subsetspread['filenumber']]:
 			#read file
 			with codecs.open(os.path.join('/Users/ps22344/Downloads/ota_0621', unicode(fileno)+".txt"), "r", "utf-8") as inputfili:
 				inputtext=inputfili.read()
 			content=adtextextractor(inputtext,item)
-			print type(content)
+			#print "original", content[:20000]
+			content=re.sub(u"(—|-)", " ", content)
+			#print header, header, "new", content[:20000]
 			contentsplit=nltk.tokenize.word_tokenize(content, language='english')
-			
-			#contentsplit=content.encode("utf-8").split()
-			#print fileno, [i.decode("utf-8") for i in contentsplit][:2000]
-			
-			#print "Before removing punctuation, this text was {} words long".format(len(contentsplit))
-			text=[i.lower() for i in contentsplit if not re.match(r"\d+", i)]
-
+			text= [i.lower() for i in contentsplit if not re.match(r"\d+", i)]
 			text= [re.sub(r"('s|s|s's|ed)\b", "", i) for i in text if i not in string.punctuation]
-			#print text[:2000]
-			print fileno, text[:2000]
-			#print "After removing punctuation, this text was {} words long".format(len(text))
+			text= [re.sub(r"(\.+|'+|,+|\*+)", "", i, re.UNICODE) for i in text]
 			for word in text:
 				perioddicti[word].append(word)
-						
 		print header, "Number of words in perioddicti is", len(perioddicti)
+		perioddicti_ment= {k:v for k,v in perioddicti.items() if any (re.match("^"+regex+"$",k) for regex in yeslist_words)}
+		#output = { i : [ r.match(i).group(0) for r in ref_re if r.match(i) ] for i in input }
+		#perioddicti_ment={e:perioddicti[k] for e in yesdict if re.match(e,k)}
+		#perioddicti_ment={k:[e for e in yesdict if re.match("^"+e+"$", k)] for k in perioddicti.keys()}
+		print header, perioddicti_ment		
+	# 		
+# 			
+# 			
+# 			
+# 		perioddicti_ment={e:[key for key in perioddicti if re.match("^"+e+"$", key) ] for e in yesdict}
+# 		
+# 		perioddicti_ment={k:v for k,v in perioddicti.items if [re.match("^"+e+"$", k) for e in yesdict.keys()]}
+# 		
+# 		[key for key in perioddicti if re.match("^"+e+"$", key) ] for e in yesdict}
 		perioddicti_nos={k:len(v) for k,v in perioddicti.items()}
 		hapaxdicti= {k:v for k,v in perioddicti_nos.items() if v == 1}
-		# f=codecs.open(os.path.join("outputfiles", unicode(fileno)+"________________chartest.txt"), "w", "utf-8")
-#   		f.write(" ".join(hapaxdicti.keys()))
-#   		f.close()
 		dictwriter(unicode(item)+"to"+unicode(item+interval-1)+"_hapaxdict.txt", hapaxdicti)
 		dictwriter(unicode(item)+"to"+unicode(item+interval-1)+"_dict.txt", perioddicti_nos)
 		for entry in perioddicti:
@@ -120,155 +127,66 @@ def main(inputspread, start_time, end_time, interval):
 	dictwriter(unicode(start_time)+"to"+unicode(end_time)+"overall_dict.txt", overalldicti)
 	print "Making hapaxdicti"	
 	overallhapax={k:v for k,v in overalldicti.items() if sum(v.values()) == 1}
-	print overallhapax
 	print "Overall hapax has {} entries".format(len(overallhapax))
 	print "Write to file"
 	dictwriter(unicode(start_time)+"to"+unicode(end_time)+"overall_hapaxdict.txt", overallhapax)
+	hapaxspread=pandas.DataFrame()
+	# year:list 0f hapaxes
+	
+def hapax_stats(hapax_file, allwords_file):
+	hapax_periods=defaultdict()
+	allwords_periods=defaultdict()
+	with open(hapax_file) as inputfile:
+		hapaxes=json.load(inputfile, encoding='utf-8')
+	with open(allwords_file) as inputfile:
+		allwords=json.load(inputfile, encoding='utf-8')
+	
+	#this is set comprehension, cf here: http://stackoverflow.com/questions/30331907/list-comprehension-check-if-item-is-unique
+	periods={v.keys()[0] for k,v in allwords.items()}
+	print periods
+	for p in periods:
+		hapax_periods[p]=len([e for e in hapaxes.values() if e.get(p, None)])
+	hapax_periods[u'overall']=sum(hapax_periods.values())
+	print hapax_periods
+	for p in periods:
+		allwords_periods[p]=[e.values()[0] for e in allwords.values() if e.get(p, None)]
+		allwords_periods[p]=sum(allwords_periods[p])
+	allwords_periods[u'overall']=sum(allwords_periods.values())
+	print allwords_periods
+	for item in hapax_periods:
+		print type(item)
+	hapaxspread=pandas.DataFrame(hapax_periods, index=["hapaxes"])
+
+	allwordspread=pandas.DataFrame(allwords_periods, index=["allwords"])
+	print hapaxspread, header
+	print allwordspread, header
+	
+	# fullspread=pandas.concat([hapaxspread, allwordspread])
+# 	fullspread['freqpermil']=fullspread['hapaxes']/fullspread['allwords']
+# 	print header, fullspread
+	
+	
+	fullspread=pandas.DataFrame(index=hapax_periods.keys())
+	fullspread['hapaxes']=hapax_periods.values()
+	fullspread['allwords']=allwords_periods.values()
+	print header, fullspread
+	fullspread['freqpermil']=fullspread['hapaxes']/fullspread['allwords']*1000000
+	print header, fullspread
+		
+		
+#hapax_stats('/Users/ps22344/Downloads/ota-master/1700to1800overall_hapaxdict.txt.json', '/Users/ps22344/Downloads/ota-master/1700to1800overall_dict.txt.json')
+		
+	
+		
+	#iterate over periods to 
+	# get total number of words
+	# number of hapaxes
+	#then totaltotal number of hapaxes, totaltotal number of words
 
 
 
-main(inputi, 1700, 1720, 10)
+main(inputi, 1700, 1800, 10)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# #setting up the output file
-# outputfile="output_gaddlist_0714"
-# print outputfile
-# 
-# #reading in the yeslist, nolist or whatever. these are the words to iterate over/search for
-# yeslist=[]
-# f=codecs.open("/Users/ps22344/Downloads/ota-master/paperstuff/gadds_yeslist_withzeros_0713.txt_pandas_0to1700.txt", "r", "utf-8")
-# for line in f:
-# 	yeslist.append(line.rstrip("\n").split("\t"))
-# 	
-# f.close()
-# 
-# #WATCH THIS SETTING
-# yeslist_words=[i[1] for i in yeslist]
-# 
-# print yeslist_words, "\n"
-# print "we have {} words\n".format(len(yeslist_words))
-# #this is the list with the files/books we're using
-# goodfiles=[]
-# f=open("/Users/ps22344/Downloads/ota-master/paperstuff/goodfiles_0620_16.txt", "r")
-# 
-# for line in f:
-# 	goodfiles.append(line.rstrip("\n"))
-# 
-# f.close()
-# print "we have {} files\n".format(len(goodfiles))
-# # some helper funcs
-# 
-# def tagextractor(text, tag, fili):
-#     regexstring="<"+tag+"=(.*?)>"
-#     result=re.findall(regexstring, text, re.DOTALL)
-#     if len(result) != 1:
-#         print "alarm in tagextractor", fili, result
-#     return result[0]
-# 
-# def adtextextractor(text, fili):
-#     regexstring="<text>(.*?)</text>"
-#     result=re.findall(regexstring, text, re.DOTALL)
-#     if len(result) != 1:
-#         print "alarm in adtextextractor", fili, result
-#     return result[0]
-# 
-# #CAREFUL!! THIS VARIES DEP ON WHERE THE FILE WSS OUTPUT
-# #
-# 
-# 
-# dicti={i:0 for i in yeslist_words}
-# 
-# for period in range(1700, 1800, 10):
-# 	print period
-# 
-# 
-# 
-# output0=codecs.open(outputfile, "a", "utf-8")
-# 
-# #output column names
-# cols=['uniq', 'filenumber', 'otanumber', 'pubdate', 'genre', 'title', 'wordcount']
-# output0.write("\t".join(cols)+"\t")
-# output0.write("\t".join(yeslist_words)+"\n")
-# output0.close()
-# #the actual reader
-# for item in goodfiles:
-# 	try:
-# 		#we open the corpus fils&read it
-# 		#/Users/ps22344/Downloads
-# 		output1=codecs.open(outputfile, "a", "utf-8")
-# 		finput=codecs.open(os.path.join("/Users","ps22344","Downloads", "ota_0621",str(item)+".txt"), "r", "utf-8")
-# 		text=finput.read()
-# 		#we get the metadata
-# 		otanumber=tagextractor(text, "otanumber", item )
-# 		filenumber=tagextractor(text, "no", item )
-# 		pubdate=tagextractor(text, "pubdate", item )
-# 		genre=tagextractor(text, "genre1", item )
-# 		title=tagextractor(text, "otatitle", item )
-# 		content=adtextextractor(text,item)
-# 		contentsplit=nltk.word_tokenize(content)
-# 		print "Before removing punctuation, this text was {} words long".format(len(contentsplit))
-# 		text=[i for i in contentsplit if i not in string.punctuation]
-# 		print "After removing punctuation, this text was {} words long".format(len(text))
-# 		#print len(contentsplit)
-# 		
-# 		#setting up the list for the findings for each text
-# 		results=[]
-# 		#a list of the words well be searching for, to be used in regex
-# 		#write the item from ll, write filenumber etc, add tab for separator
-# 		outputlist=[unicode(item), filenumber, otanumber, pubdate, genre, title, unicode(len(text))]
-# 		output1.write("\t".join(outputlist)+"\t")
-# 		#iterate over all metadata
-# 		output1.close()
-# 		for thing in yeslist_words:
-# 			#no suffix
-# 			words=re.findall(r"\b("+thing+"\'?)",content)
-# 			#yes suffix
-# 			#words=re.findall(r"\b((?:dis|mis|re|un)?"+thing+"\'?)",content)
-# 			dicti[thing]=dicti[thing]+len(words)
-# 			results.append(words)
-# 		print "reading", item, filenumber
-# 
-# 		output3=codecs.open(outputfile, "a", "utf-8")
-# 		output3.write("\t".join([str(len(i)) for i in results])+"\n")
-# 		output3.close()
-# 		logout=codecs.open(outputfile+"_log.txt", "a", "utf-8")
-# 		logout.write(str(results)+"\n")
-# 		logout.close()
-# 
-# 	except IOError, err:
-# 		print "Error", err
-# dictiout=open(outputfile+"_dicti.txt", "w")
-# sortdict=sorted(dicti.items(), key=lambda x: x[1], reverse=True)
-# dictiout.write("\n".join([str(i) for i in sortdict]))
-# print sortdict
-# dictiout.close()
-# 
-# 
-# 
-# #for spreadsheet
-# for item in yeslist_words:
-#     print item
     
 later=time.time()
 runtime=later-now
